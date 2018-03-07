7222f30bc11015b9b8a0b88b0e04d9b22c9da192
Draconems <34941327+Draconems@users.noreply.github.com>
2018-03-06 18:01:59 -0600

    Ejemplo de una aplicacion android

---
+++ Clave/app/src/main/java/com/calculadora2/example/luis/clave/segundaC.java
@@ -0,0 +1,27 @@
+package com.calculadora2.example.luis.clave;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by Luis on 04/04/2016.
+ */
+public class segundaC extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundo);
+
+    }
+
+
+}
